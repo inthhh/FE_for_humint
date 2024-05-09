3500c2ca1946c0c92fc79e54dacb5abadbcf5683
inthhh <glass005533@gmail.com>
2024-05-09 14:04:13 +0900

    fix: 경로 수정

--- humint-front/src/App.tsx
+++ humint-front/src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './Home';
-import Table from './TableData/Table';
+import Table from './tabledata/Table';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
--- humint-front/src/Home.tsx
+++ humint-front/src/Home.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import { useState, useEffect } from 'react';
 import './Home.css';
 import axios from 'axios';
-import Table from './TableData/Table';
+import Table from './tabledata/Table';
 import { toHaveDescription } from '@testing-library/jest-dom/matchers';
 import store from './redux/store';
 import { Provider } from 'react-redux';
