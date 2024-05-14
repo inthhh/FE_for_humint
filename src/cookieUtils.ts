b67f4da0fb4ae2393f8cbe820f714071482bfbdd
lob <monjk557@gmail.com>
2024-05-14 16:29:32 +0900

    refactor : directory refactoring

---
+++ src/cookieUtils.ts
@@ -0,0 +1,22 @@
+// cookieUtils.ts
+
+import {Cookies} from 'react-cookie';
+
+export function setCookie(name: string, value: string, days: number) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+  }
+  
+  export function getCookie(name: string): string | null {
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const cookies = decodedCookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      if (cookie.startsWith(name + "=")) {
+        return cookie.substring(name.length + 1);
+      }
+    }
+    return null;
+  }
