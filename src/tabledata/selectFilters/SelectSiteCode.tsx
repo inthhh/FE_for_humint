41da2fe5adb3b960106b20fe7e7142b61986a51f
inthhh <glass005533@gmail.com>
2024-06-11 17:45:27 +0900

    feat: site code 필터에 sec 추가

--- src/tabledata/selectFilters/SelectSiteCode.tsx
+++ src/tabledata/selectFilters/SelectSiteCode.tsx
@@ -12,7 +12,7 @@ function SelectSiteCode() {
     'in','iq_ar','iq_ku','iran','it','jp','kz_kz','kz_ru','latin','latin_en',
     'lb','levant','levant_ar','lt','lv','mk','mm','mn','mx','my',
     'n_africa','nl','no','nz','pe','ph','pk','pl','ps','pt',
-    'py','ro','rs','ru','sa','sa_en','se','sg','si','sk',
+    'py','ro','rs','ru','sa','sa_en','se','sec','sg','si','sk',
     'th','tr','tw','ua','uk','us','uy','uz_ru','uz_uz','vn',
     'za'
   ]
