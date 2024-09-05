2cda8e48434b73f50527604faf0cac4c6ea5743e
inthhh <glass005533@gmail.com>
2024-09-05 22:52:37 +0900

    code는 main 브랜치에서 열람이 가능합니다.

--- src/redux/actions/userAction.ts
+++
@@ -1,7 +0,0 @@
-import { AnyAction } from "redux";
-import { SET_MYNAME } from "../constants/userConstant";
-
-export const myName = (myname: string | null): AnyAction => ({
-    type: SET_MYNAME,
-    payload: myname,
-});
